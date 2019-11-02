915e655112fd664de8f62583a39451135f58f259
Turi-byte <breldanmuturi001@gmail.com>
2019-11-02 23:54:33 +0300

    Init

---
+++ app/src/main/java/turi/d/dogs/view/MainActivity.kt
@@ -0,0 +1,14 @@
+
+package turi.d.dogs.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import turi.d.dogs.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
